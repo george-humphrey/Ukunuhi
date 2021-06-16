ba3d8099a56b6f03f4329557351b2b30641d686e
George Humphrey <george.p.humphrey@gmail.com>
2021-06-16 00:13:51 -0400

    added in keyboard translation

--- client/src/components/Keyboard.jsx
+++ client/src/components/Keyboard.jsx
@@ -4,8 +4,19 @@ const Keyboard = (props) => {
 
   return (
     <div id='keyboard'>
+      <div id='blackKeys'>
+        {[1, 3, 6, 8, 10, 13, 15, 18].map((index) => {
+          return (<div className='key blackKey' id={`key${index}`} key={`k${index}`}></div>)
+        })}
+      </div>
+      <div id='whiteKeys'>
+        {[0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19].map((index) => {
+          return (<div className='key whiteKey' id={`key${index}`} key={`k${index}`}></div>)
+        })}
+      </div>
     </div>
   )
 }
 
 export default Keyboard;
+
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
 import Fretboard from './components/Fretboard.jsx';
+import Keyboard from './components/Keyboard.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       chord: 'C',
-      strings: [0, 0, 0, 2]
+      strings: [0, 0, 0, 3]
     }
-    this.clickString = this.clickString.bind(this)
+    this.clickString = this.clickString.bind(this);
+    this.pianofy = this.pianofy.bind(this);
   }
 
   clickString(string, fret) {
@@ -21,10 +23,26 @@ class App extends React.Component {
     } else {
       strings[string - 1] = fret;
     }
-    this.setState({ strings });
+    this.setState({ strings }, () => this.pianofy());
+
+  }
+
+  pianofy() {
+    // standard tuning (i.e., gCEA)
+    let standardTuning = [7, 0, 4, 9];
+    let strings = this.state.strings;
+    let currentKeys = standardTuning.map((x, i) => {
+      return x + strings[i];
+    })
+
+    $('.key').removeClass('selectedKey');
+    currentKeys.forEach((index) => {
+      $(`#key${index}`).addClass('selectedKey');
+    })
   }
 
   componentDidMount() {
+    this.pianofy();
   }
 
   render() {
@@ -32,6 +50,7 @@ class App extends React.Component {
       <div id='app'>
         <h1>Ukunuhi</h1>
         <Fretboard strings={this.state.strings} click={this.clickString} />
+        <Keyboard />
       </div >
     )
   }
